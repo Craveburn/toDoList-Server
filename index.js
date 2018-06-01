8589cce12ea8ff32b5d7934d72aed94ccd41d391
Cameron Rabunn <raburncameron@gmail.com>
2018-05-31 18:24:59 -0600

    update on express server

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
-app.get('/', (req, res))
+app.use(bodyParser.json())
+
+const inMemoryDatabase = {
+    users: [
+
+    ]
+}
+
+app.get('/list', (req, res) => {
+    res.send(inMemoryDatabase.users)
+})
+
+app.post('/list', (req, res) => {
+    const newItem = req.body
+    inMemoryDatabase.users.push(newItem)
+    res.send('Added new item: ' + newItem)
+})
 
 app.listen('3001', () => {
     console.log("we are live")
