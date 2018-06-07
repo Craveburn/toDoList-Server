e40ea220cc914ccf60f5172a97e064150a9d37ff
Cameron Rabunn <raburncameron@gmail.com>
2018-06-06 20:35:48 -0600

    added a delete and put function to my server

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
 const bodyParser = require('body-parser')
+const db = require('monk')(process.env.mongoconnstring)
+const listCollection = db.get('list')
 
 const app = express();
 
-app.use(bodyParser.json())
-
-const inMemoryDatabase = {
-    users: [
-
-    ]
-}
-
-app.get('/list', (req, res) => {
-    res.send(inMemoryDatabase.users)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
 })
 
-app.post('/list', (req, res) => {
+app.use(bodyParser.json())
+
+app.get('/list', async (req, res) => {
+    const items = await listCollection.find({})
+    res.send(items)
+})
+
+app.post('/list', async (req, res) => {
     const newItem = req.body
-    inMemoryDatabase.users.push(newItem)
-    res.send('Added new item: ' + newItem)
+    const savedItem = await listCollection.insert(newItem)
+    res.send(savedItem)
+    console.log("hit the post")
+})
+
+app.delete('/list', async (req, res) => {
+    const deleteItem = req.body.id
+    console.log("delted item", deleteItem)
+    const beGone = await listCollection.remove(deleteItem)
+    res.send(deleteItem)
+
+})
+
+app.put('/list', async (req, res) => {
+    const updateItem = req.body
+    console.log(updateItem)
+    const editThis = await listCollection.update({_id: updateItem._id}, updateItem)
+    res.send(updateItem)
 })
 
 app.listen('3001', () => {
